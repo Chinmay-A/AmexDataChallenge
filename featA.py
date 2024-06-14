d42107371311d3840ea5d374e79bb8cb45a0f354
Chinmay-A <chinmayaons1@gmail.com>
2024-06-14 20:17:30 +0530

    dataset ready for training

--- featA.py
+++ featA.py
@@ -18,9 +18,6 @@ class Data:
         self.TEST['match_dt']=pd.to_datetime(self.TEST['match_dt'], format='%Y-%m-%d')
         self.MATCHLEVEL['match_dt']=pd.to_datetime(self.MATCHLEVEL['match_dt'], format='%Y-%m-%d')
 
-        print("master")
-        print(self.MATCHLEVEL.columns)
-
         self.momentum_score_arr=[]
         self.location_score_arr=[]
         self.batting_score_arr=[]
@@ -31,41 +28,21 @@ class Data:
         self.train=self.TRAIN
         self.test=self.TEST
         self.matchlevel=self.MATCHLEVEL
-        
 
     def initialize(self,date):
-<<<<<<< HEAD
-
-        #pd_date_time=pd.to_datetime(date,format='%Y-%m-%d')
-        pd_date_time=date[0]
-        #print(pd_date_time)
-        
-        self.bowl=self.BOWLERS[self.BOWLERS['match_dt']<pd_date_time]
-        self.bat=self.BATSMEN[self.BATSMEN['match_dt']<pd_date_time]
-        self.train=self.TRAIN[self.TRAIN['match_dt']<pd_date_time]
-        self.test=self.TEST[self.TEST['match_dt']<pd_date_time]
-        self.matchlevel=self.MATCHLEVEL[self.MATCHLEVEL['match_dt']<pd_date_time]
-        
-=======
-        date = date[0]
+        # date = date[0]
         
         self.bowl=self.BOWLERS[self.BOWLERS['match_dt']<date]
         self.bat=self.BATSMEN[self.BATSMEN['match_dt']<date]
         self.train=self.TRAIN[self.TRAIN['match_dt']<date]
         self.test=self.TEST[self.TEST['match_dt']<date]
         self.matchlevel=self.MATCHLEVEL[self.MATCHLEVEL['match_dt']<date]
->>>>>>> 2d3521079ff23bd3f02d8f5004f633a3fdc1ead0
 
         self.bowl=self.bowl.sort_values(by=['match_dt'],ascending=False)
         self.bat=self.bat.sort_values(by=['match_dt'],ascending=False)
         self.train=self.train.sort_values(by=['match_dt'],ascending=False)
         self.test=self.test.sort_values(by=['match_dt'],ascending=False)
         self.matchlevel=self.matchlevel.sort_values(by=['match_dt'],ascending=False)
-<<<<<<< HEAD
-        print("master")
-        print(self.matchlevel.columns)
-=======
->>>>>>> 2d3521079ff23bd3f02d8f5004f633a3fdc1ead0
     
     def process_row(self,row):
 
@@ -83,15 +60,21 @@ class Data:
 
         self.momentum_score_arr.append(self.momentum_score(row))
         self.location_score_arr.append(self.location_score(row))
-        self.batting_score_arr.append(self.batting_score(teamA)/self.batting_score(teamB))
-        self.bowling_score_arr.append(self.bowling_score(teamA)/self.bowling_score(teamB))
+        if self.batting_score(teamB)==0:
+            self.batting_score_arr.append(1)
+        else:
+            self.batting_score_arr.append(self.batting_score(teamA)/self.batting_score(teamB))
+        if self.bowling_score(teamB)==0:
+            self.bowling_score_arr.append(1)
+        else:
+            self.bowling_score_arr.append(self.bowling_score(teamA)/self.bowling_score(teamB))
     
     def location_score(self,row):
 
         toss_winner=row['toss winner']
         toss_decision=row['toss decision']
 
-        relevant_games=self.matchlevel[self.matchlevel['ground_id']==row['ground_id'][0]]
+        relevant_games=self.matchlevel[self.matchlevel['ground_id']==row['ground_id']]
 
         if(len(relevant_games)>10):
             relevant_games=relevant_games.head(10)
@@ -100,9 +83,9 @@ class Data:
         batfirstwins=len(relevant_games[relevant_games['by']=='wickets'])
 
         if(bowlfirstwins>batfirstwins):
-            if(toss_winner==row['team1'][0] and toss_decision=='field'):
+            if(toss_winner==row['team1'] and toss_decision=='field'):
                 return 1
-            elif(toss_winner==row['team2'][0] and toss_decision=='bat'):
+            elif(toss_winner==row['team2'] and toss_decision=='bat'):
                 return 1
         else:
             if(toss_winner==row['team1'] and toss_decision=='bat'):
@@ -114,16 +97,16 @@ class Data:
     
     def momentum_score(self,row):
 
-        relevantgamesA=self.matchlevel[(self.matchlevel['team1_id']==row['team1_id'][0]) | (self.matchlevel['team2_id']==row['team1_id'][0])]
-        relevantgamesB=self.matchlevel[(self.matchlevel['team1_id']==row['team2_id'][0]) | (self.matchlevel['team2_id']==row['team2_id'][0])]
+        relevantgamesA=self.matchlevel[(self.matchlevel['team1_id']==row['team1_id']) | (self.matchlevel['team2_id']==row['team1_id'])]
+        relevantgamesB=self.matchlevel[(self.matchlevel['team1_id']==row['team2_id']) | (self.matchlevel['team2_id']==row['team2_id'])]
         
         if(len(relevantgamesA)>10):
             relevantgamesA=relevantgamesA.head(10)
         if(len(relevantgamesB)>10):
             relevantgamesB=relevantgamesB.head(10)
         
-        team1wins=len(relevantgamesA[relevantgamesA['winner_id']==row['team1_id'][0]])
-        team2wins=len(relevantgamesB[relevantgamesB['winner_id']==row['team2_id'][0]])
+        team1wins=len(relevantgamesA[relevantgamesA['winner_id']==row['team1_id']])
+        team2wins=len(relevantgamesB[relevantgamesB['winner_id']==row['team2_id']])
 
         if(team2wins==0):
             return 1
@@ -132,7 +115,7 @@ class Data:
     
     def batting_score(self,teamid):
 
-        relevantgamesA=self.matchlevel[self.matchlevel['team1_id']==teamid or self.matchlevel['team2_id']==teamid]
+        relevantgamesA=self.matchlevel[(self.matchlevel['team1_id']==teamid) | (self.matchlevel['team2_id']==teamid)]
 
         if(len(relevantgamesA)>10):
             relevantgamesA=relevantgamesA.head(10)
@@ -140,7 +123,6 @@ class Data:
         relevantgamesA.reset_index(inplace=True)
         
         runsbyA=[]
-
         for i in range(len(relevantgamesA)):
 
             if(relevantgamesA['team1_id'][i]==teamid and relevantgamesA['team1'][i]==relevantgamesA['toss winner'][i] and relevantgamesA['toss decision'][i]=='bat'):
@@ -150,15 +132,20 @@ class Data:
             elif(relevantgamesA['team1_id'][i]==teamid):
                 runsbyA.append(relevantgamesA['inning2_runs'][i])
             elif(relevantgamesA['team2_id'][i]==teamid and relevantgamesA['team2'][i]==relevantgamesA['toss winner'][i] and relevantgamesA['toss decision'][i]=='bat'):
-                runsbyA.append(relevantgamesA['innigs1_runs'][i])
+                runsbyA.append(relevantgamesA['inning1_runs'][i])
             elif(relevantgamesA['team2_id'][i]==teamid and relevantgamesA['team1'][i]==relevantgamesA['toss winner'][i] and relevantgamesA['toss decision'][i]=='field'):
-                runsbyA.append(relevantgamesA['innigs1_runs'][i])
+                runsbyA.append(relevantgamesA['inning1_runs'][i])
             else:
-                runsbyA.append(relevantgamesA['innings2_runs'][i])
-        
+                runsbyA.append(relevantgamesA['inning2_runs'][i])
+
+        runsbyA = [float(i) for i in runsbyA]
+        if len(runsbyA) <= 1 : return 0
+
         stddev=stats.stdev(runsbyA)
         mean=stats.mean(runsbyA)
 
+        if stddev == 0: return 0
+        
         if(len(runsbyA)<3):
 
             return (runsbyA[0]-mean)/stddev
@@ -172,7 +159,7 @@ class Data:
     
     def bowling_score(self,teamid):
 
-        relevantgamesA=self.matchlevel[(self.matchlevel['team1_id']==teamid) or (self.matchlevel['team2_id']==teamid)]
+        relevantgamesA=self.matchlevel[(self.matchlevel['team1_id']==teamid) | (self.matchlevel['team2_id']==teamid)]
 
         if(len(relevantgamesA)>10):
             relevantgamesA=relevantgamesA.head(10)
@@ -190,14 +177,19 @@ class Data:
             elif(relevantgamesA['team1_id'][i]==teamid):
                 wicketsbyA.append(relevantgamesA['inning2_wickets'][i])
             elif(relevantgamesA['team2_id'][i]==teamid and relevantgamesA['team2'][i]==relevantgamesA['toss winner'][i] and relevantgamesA['toss decision'][i]=='field'):
-                wicketsbyA.append(relevantgamesA['innigs1_wickets'][i])
+                wicketsbyA.append(relevantgamesA['inning1_wickets'][i])
             elif(relevantgamesA['team2_id'][i]==teamid and relevantgamesA['team1'][i]==relevantgamesA['toss winner'][i] and relevantgamesA['toss decision'][i]=='bat'):
-                wicketsbyA.append(relevantgamesA['innigs1_wickets'][i])
+                wicketsbyA.append(relevantgamesA['inning1_wickets'][i])
             else:
-                wicketsbyA.append(relevantgamesA['innings2_wickets'][i])
+                wicketsbyA.append(relevantgamesA['inning2_wickets'][i])
         
+        wicketsbyA = [float(i) for i in wicketsbyA]
+
+        if len(wicketsbyA) <= 1: return 0
+
         stddev=stats.stdev(wicketsbyA)
         mean=stats.mean(wicketsbyA)
+        if stddev == 0: return 0
 
         if(len(wicketsbyA)<3):
 
@@ -216,9 +208,9 @@ class Data:
 
         #self.TRAIN.reset_index(inplace=True)
 
-        for i in range(1):
-            #print(i)
-            curr_row=self.TRAIN.iloc[[i]]
+        for i in range(n):
+            # print(i)
+            curr_row=self.TRAIN.iloc[i]
             #print(curr_row)
             self.process_row(curr_row) 
 
@@ -228,12 +220,3 @@ class Data:
         self.TRAIN['bowling_score']=self.bowling_score_arr
 
         self.TRAIN.to_csv('training_set.csv',index=False)
-
-
-
-
-        
-
-
-
-
